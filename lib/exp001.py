f05bf82ecd5cc5b6344564104c7f57180bbdacf6
Kazuaki Tokunaga <32542500+KazuakiTokunaga@users.noreply.github.com>
2023-11-28 21:50:54 +0900

    update

--- lib/exp001.py
+++ lib/exp001.py
@@ -22,6 +22,7 @@ import datetime
 
 from utils.utils import Logger, WriteSheet
 from utils.utils import class_vars_to_dict
+from utils.essay import getEssays
 
 
 class ENV:
@@ -44,73 +45,6 @@ class RCFG:
     n_splits = 10
     preprocess_train = False
     predict = False
-
-
-# Function to construct essays copied from here (small adjustments): https://www.kaggle.com/code/yuriao/fast-essay-constructor
-def processingInputs(currTextInput):
-    essayText = ""
-    for Input in currTextInput.values:
-        # Input[0] = activity
-        # Input[1] = cursor_position
-        # Input[2] = text_change
-        # Input[3] = id
-        # If activity = Replace
-        if Input[0] == 'Replace':
-            # splits text_change at ' => '
-            replaceTxt = Input[2].split(' => ')
-            essayText = essayText[:Input[1] - len(replaceTxt[1])] + replaceTxt[1] + essayText[Input[1] - len(replaceTxt[1]) + len(replaceTxt[0]):]
-            continue
-
-        # If activity = Paste    
-        if Input[0] == 'Paste':
-            essayText = essayText[:Input[1] - len(Input[2])] + Input[2] + essayText[Input[1] - len(Input[2]):]
-            continue
-
-        # If activity = Remove/Cut
-        if Input[0] == 'Remove/Cut':
-            essayText = essayText[:Input[1]] + essayText[Input[1] + len(Input[2]):]
-            continue
-
-        # If activity = Move...
-        if "M" in Input[0]:
-            # Gets rid of the "Move from to" text
-            croppedTxt = Input[0][10:]              
-            # Splits cropped text by ' To '
-            splitTxt = croppedTxt.split(' To ')              
-            # Splits split text again by ', ' for each item
-            valueArr = [item.split(', ') for item in splitTxt]              
-            # Move from [2, 4] To [5, 7] = (2, 4, 5, 7)
-            moveData = (int(valueArr[0][0][1:]), int(valueArr[0][1][:-1]), int(valueArr[1][0][1:]), int(valueArr[1][1][:-1]))
-            # Skip if someone manages to activiate this by moving to same place
-            if moveData[0] != moveData[2]:
-                # Check if they move text forward in essay (they are different)
-                if moveData[0] < moveData[2]:
-                    essayText = essayText[:moveData[0]] + essayText[moveData[1]:moveData[3]] +\
-                    essayText[moveData[0]:moveData[1]] + essayText[moveData[3]:]
-                else:
-                    essayText = essayText[:moveData[2]] + essayText[moveData[0]:moveData[1]] +\
-                    essayText[moveData[2]:moveData[0]] + essayText[moveData[1]:]
-            continue                
-
-        # If activity = input
-        essayText = essayText[:Input[1] - len(Input[2])] + Input[2] + essayText[Input[1] - len(Input[2]):]
-    return essayText
-
-
-def getEssays(df):
-    # Copy required columns
-    textInputDf = copy.deepcopy(df[['id', 'activity', 'cursor_position', 'text_change']])
-    # Get rid of text inputs that make no change
-    textInputDf = textInputDf[textInputDf.activity != 'Nonproduction']     
-    # construct essay, fast 
-    tqdm.pandas()
-    essay=textInputDf.groupby('id')[['activity','cursor_position', 'text_change']].progress_apply(
-        lambda x: processingInputs(x))      
-    # to dataframe
-    essayFrame=essay.to_frame().reset_index()
-    essayFrame.columns=['id','essay']
-    # Returns the essay series
-    return essayFrame
 
 
 def q1(x):
