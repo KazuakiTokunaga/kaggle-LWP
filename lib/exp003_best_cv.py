4bf636821a550e527db501b8b4c98980dde9866d
Kazuaki Tokunaga <32542500+KazuakiTokunaga@users.noreply.github.com>
2023-12-31 14:53:03 +0900

    update

--- lib/exp003_best_cv.py
+++ lib/exp003_best_cv.py
@@ -290,19 +290,21 @@ def dev_feats(df):
     )
     feats = feats.join(temp, on='id', how='left')
 
-
-    temp = df.group_by('id').agg(
-        ((pl.col('activity')=='Input') & (pl.col('text_change')=="q")).sum().alias('activity_Input_q_cnt'),
-        ((pl.col('activity')=='Input') & (pl.col('text_change')==" ")).sum().alias('activity_Input_space_cnt')
-    )
-    feats = feats.join(temp, on='id', how='left')
     feats = feats.with_columns(
-        # (pl.col('activity_Remove/Cut_cnt') / pl.col('activity_Input_cnt')).alias('activity_Input_remove_rate'),
-        (pl.col('activity_Input_q_cnt') / pl.col('activity_Input_cnt')).alias('activity_Input_q_rate'),
-        (pl.col('activity_Input_space_cnt') / pl.col('activity_Input_cnt')).alias('activity_Input_space_rate'),
-        # (pl.col('down_event_Shift_cnt') / pl.col('activity_Input_cnt')).alias('activity_Input_shift_rate')
-    ).drop('activity_Input_q_cnt', 'activity_Input_space_cnt')
-
+        (pl.col('event_id_max_25min') / pl.col('event_id_max')).alias('event_id_max_25min_rate'),
+        (pl.col('event_id_max_20min') / pl.col('event_id_max')).alias('event_id_max_20min_rate'),
+        (pl.col('cursor_position_max_25min') / pl.col('cursor_position_max')).alias('cursor_position_max_25min_rate'),
+        (pl.col('cursor_position_max_20min') / pl.col('cursor_position_max')).alias('cursor_position_max_20min_rate'),
+        (pl.col('word_count_max_25min') / pl.col('word_count_max')).alias('word_count_max_25min_rate'),
+        (pl.col('word_count_max_20min') / pl.col('word_count_max')).alias('word_count_max_20min_rate'),
+    ).drop(
+        'event_id_max_25min', 
+        'event_id_max_20min', 
+        'cursor_position_max_25min', 
+        'corsor_position_max_20min', 
+        'word_count_max_25min',
+        'word_count_max_20min'
+    )
 
     return feats
 
@@ -628,7 +630,7 @@ class Runner():
         # logger.info('Add CountVectorizer features.')
         # feats = feats.merge(get_countvectorizer_features(essays, mode=mode), on='id', how='left')
 
-        # feats['comma_sent_len_rate'] = feats['down_event_comma_cnt'] / feats['sent_len_sum']
+        feats['comma_sent_len_rate'] = feats['down_event_comma_cnt'] / feats['sent_len_sum']
 
         return feats
 
