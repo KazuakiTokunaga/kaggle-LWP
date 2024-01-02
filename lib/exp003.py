2058cc07000ac37e2329830127af84e5aabeefca
Kazuaki Tokunaga <32542500+KazuakiTokunaga@users.noreply.github.com>
2024-01-02 14:11:33 +0900

    update

--- lib/exp003.py
+++ lib/exp003.py
@@ -254,8 +254,8 @@ def dev_feats(df):
         pl.count('P-bursts').name.suffix('_count'),
         pl.median('P-bursts').name.suffix('_median'), 
         pl.max('P-bursts').name.suffix('_max'),
-        pl.first('P-bursts').name.suffix('_first'), # good for LB, bad for CV
-        pl.last('P-bursts').name.suffix('_last'), # good for LB, bad for CV
+        # pl.first('P-bursts').name.suffix('_first'), # good for LB, bad for CV
+        # pl.last('P-bursts').name.suffix('_last'), # good for LB, bad for CV
     )
     feats = feats.join(temp, on='id', how='left') 
 
@@ -287,8 +287,8 @@ def dev_feats(df):
         pl.std('R-bursts').name.suffix('_std'), 
         pl.median('R-bursts').name.suffix('_median'), 
         pl.max('R-bursts').name.suffix('_max'),
-        pl.first('R-bursts').name.suffix('_first'), # good for LB, bad for CV
-        pl.last('R-bursts').name.suffix('_last'), # good for LB, bad for CV
+        # pl.first('R-bursts').name.suffix('_first'), # good for LB, bad for CV
+        # pl.last('R-bursts').name.suffix('_last'), # good for LB, bad for CV
     )
     feats = feats.join(temp, on='id', how='left')
 
@@ -451,14 +451,14 @@ def word_feats_v2(df):
     df_words = df.groupby('id').agg(
         word_feats_apostrophe_cnt = ('word', lambda x: ((x.str.endswith("'q")) & (x.str.len()<=5)).sum()),
         word_feats_long_apostrophe_cnt = ('word', lambda x: ((x.str.contains("'")) & (x.str.len()>=8)).sum()),
-        word_feats_len35_len_sum5 = ('word_len_sum5', lambda x: (x>=35).sum()),
+        # word_feats_len35_len_sum5 = ('word_len_sum5', lambda x: (x>=35).sum()),
         # word_feats_len60_len_sum10 = ('word_len_sum10', lambda x: (x>=60).sum()),
         # word_feats_median_len_sum5 = ('word_len_sum5', 'median'),
-        word_feats_median_len_sum10 = ('word_len_sum10', 'median')
+        word_feats_median_len_sum10 = ('word_len_sum10', 'median'),
         # word_feats_mean_len_sum5 = ('word_len_sum5', 'mean'),
         # word_feats_median_len_sum5 = ('word_len_sum5', 'median'),
         # word_feats_q3_len_sum5 = ('word_len_sum5', q3),
-        # word_feats_quantile90_len_sum5 = ('word_len_sum5', quantile90),
+        word_feats_quantile90_len_sum5 = ('word_len_sum5', quantile95)
         # word_feats_mean_len_sum10 = ('word_len_sum10', 'mean'),
         # word_feats_q3_len_sum10 = ('word_len_sum10', q3),
         # word_feats_quantile90_len_sum10 = ('word_len_sum10', quantile90)
